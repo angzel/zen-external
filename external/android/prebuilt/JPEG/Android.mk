85549291cb357f323a7f038e01d61efacf760833
MeherTj <lover@loverdeAir.lan>
2020-11-01 16:01:01 +0800

    fix android make

--- external/android/prebuilt/JPEG/Android.mk
+++ external/android/prebuilt/JPEG/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_jpeg_static
 LOCAL_MODULE_FILENAME := jpeg
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libjpeg.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libjpeg-android.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
 include $(PREBUILT_STATIC_LIBRARY)
--- external/android/prebuilt/PNG/Android.mk
+++ external/android/prebuilt/PNG/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_png_static
 LOCAL_MODULE_FILENAME := png
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libpng.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libpng-android.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
 include $(PREBUILT_STATIC_LIBRARY)
